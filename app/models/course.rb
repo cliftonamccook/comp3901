e8cdb65bba1a2f540066e21b2ed4224d1ca79b35
Smallz2 <dexterarron@gmail.com>
2022-03-08 23:31:00 -0400

    Fixed enum conflict

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,6 @@ class Course < ApplicationRecord
   belongs_to :department
   belongs_to :requirement, optional: true
 
-  enum level: [:one, :two, :three]
-  enum semester_offered_in: [:one, :two, :both]
+  enum level: [:level_one, :level_two, :level_three]
+  enum semester_offered_in: [:semester_one, :semester_two, :both]
 end
