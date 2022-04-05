b3b592a84a74e591c757934272006e71f8751a31
Clifton McCook <cliftonmccook@gmail.com>
2022-04-04 22:11:18 -0500

    restructured files

--- logic-api/checker.py
+++ logic-api/checker.py
@@ -1,2 +1,34 @@
 from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+from models import ProgrammeTreeNode, Course, ANDGrouping, ORGrouping, StudentRecord, TermRecord, CourseRecord
+
+
+app = FastAPI()
+
+
+class StudentData(BaseModel):
+    pass
+
+
+class ProgrammeData(BaseModel):
+    pass
+
+
+# Response Model
+class ReportData(BaseModel):
+    pass
+
+
+programmes = {}
+
+@app.post("/checker/")
+def checkprogress(SR:StudentData, Prog:ProgrammeData):
+    # construct programme tree
+    # cache tree in programmes dictionary
+    # perform check (tree walk)
+    # return report
+    return "Progress Checker under construction"
+
+
+# uvicorn checker:app --reload
---
+++ logic-api/models.py
@@ -0,0 +1,30 @@
+class ProgrammeTreeNode:
+    pass
+
+
+class Course(ProgrammeTreeNode):
+    pass
+
+
+class ANDGrouping(ProgrammeTreeNode):
+    pass
+
+
+class ORGrouping(ProgrammeTreeNode):
+    pass
+
+
+class Report:
+    pass
+
+
+class StudentRecord:
+    pass
+
+
+class TermRecord:
+    pass
+
+
+class CourseRecord:
+    pass
