4d2c110c8b714f8afb4c8b1dd5da4366e28b7978
Clifton McCook <cliftonmccook@gmail.com>
2022-04-07 16:06:02 -0500

    updated checker.py

--- logic-api/checker.py
+++ logic-api/checker.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List, Set
 from fastapi import FastAPI
 from pydantic import BaseModel
 from models import ProgrammeTreeNode, Course, ANDGrouping, ORGrouping, StudentRecord, TermRecord, CourseRecord
@@ -11,8 +11,35 @@ class StudentData(BaseModel):
     pass
 
 
+class CourseData(BaseModel):
+    name: str
+    code: str
+    semester: List[int]
+    credits: int
+    campus: str
+    faculty: str
+    department: str
+    requirements: list = []
+    corerequisite: list = []
+    antirequisite: list = []
+    description: str
+    tags: set = {}
+
+
+class GroupingData(BaseModel):
+    name: str
+    code: str
+    operation: str
+    minimum_credits: int
+    requirements: list = []
+
+
 class ProgrammeData(BaseModel):
-    pass
+    name: str
+    code: str
+    operation: str
+    minimum_credits: int
+    requirements: List[GroupingData, CourseData] = []
 
 
 # Response Model
@@ -31,13 +58,19 @@ def checkprogress():
 @app.post("/checker/")
 def checkprogress(SR:StudentData, ProgID:str):
     if programmes[ProgID] is not None:
-        # construct programme tree
-        # cache tree in programmes dictionary
-        # perform check (tree walk)
+        programme = programmes[ProgID]
+        # perform check (tree walk) on cached programme tree
         # return report
         pass
     else:
         return {"error message": "Programme not present", "error ID": 401}
 
+
+@app.post("/checker/")
+def checkprogress(SR:StudentData, Prog:ProgrammeData):
+        # construct programme tree from received ProgrammeData
+        # cache tree in programmes dictionary
+        # perform check (tree walk) on cached programme tree
+        # return report
 
 # uvicorn checker:app --reload
