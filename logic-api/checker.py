b6f2d2205523f8d46d7d5819ddb3feb703ed58ad
Clifton McCook <cliftonmccook@gmail.com>
2022-04-07 21:39:43 -0500

    edited checker.py

--- logic-api/checker.py
+++ logic-api/checker.py
@@ -7,10 +7,6 @@ from models import ProgrammeTreeNode, Course, ANDGrouping, ORGrouping, StudentRe
 app = FastAPI()
 
 
-class StudentData(BaseModel):
-    pass
-
-
 class CourseData(BaseModel):
     name: str
     code: str
@@ -23,7 +19,7 @@ class CourseData(BaseModel):
     corerequisite: list = []
     antirequisite: list = []
     description: str
-    tags: set = {}
+    tags: set = set([])
 
 
 class GroupingData(BaseModel):
@@ -37,14 +33,41 @@ class GroupingData(BaseModel):
 class ProgrammeData(BaseModel):
     name: str
     code: str
+    faculty: str
     operation: str
     minimum_credits: int
-    requirements: List[GroupingData, CourseData] = []
+    requirements: list = []
+
+
+class CourseRecordData(BaseModel):
+    name: str
+    code: str
+    grade: str
+
+
+class TermData(BaseModel):
+    term: str
+    course_records: List[CourseRecordData]
+
+
+class StudentData(BaseModel):
+    name: str
+    id: int
+    campus: str
+    faculty: str
+    programme: str
+    course_history: List[TermData]
+
+
+class AuditInput(BaseModel):
+    audit_data: list
 
 
 # Response Model
 class ReportData(BaseModel):
-    pass
+    fulfilled: bool
+    credits: int
+    incomplete: list = []
 
 
 programmes = {}
@@ -55,22 +78,25 @@ def checkprogress():
     return "Progress Checker under construction"
 
 
-@app.post("/checker/")
-def checkprogress(SR:StudentData, ProgID:str):
-    if programmes[ProgID] is not None:
-        programme = programmes[ProgID]
-        # perform check (tree walk) on cached programme tree
-        # return report
-        pass
-    else:
-        return {"error message": "Programme not present", "error ID": 401}
+# @app.post("/checker/")
+# def checkprogress(SR:StudentData, ProgID:str):
+#     if programmes[ProgID] is not None:
+#         programme = programmes[ProgID]
+#         # perform check (tree walk) on cached programme tree
+#         # return report
+#         pass
+#     else:
+#         return {"error message": "Programme not present", "error ID": 401}
 
 
 @app.post("/checker/")
-def checkprogress(SR:StudentData, Prog:ProgrammeData):
+def checkprogress(payload:AuditInput):
         # construct programme tree from received ProgrammeData
         # cache tree in programmes dictionary
         # perform check (tree walk) on cached programme tree
         # return report
+        student_record = payload.audit_data[0]
+        programme = payload.audit_data[1]
+        return student_record
 
 # uvicorn checker:app --reload
