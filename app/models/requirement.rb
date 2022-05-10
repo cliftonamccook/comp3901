47e0240a5cfbc56fe6e19961ccdff481dc647764
Smallz2 <dexterarron@gmail.com>
2022-05-09 23:55:21 -0400

    Update requirement

--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -5,7 +5,7 @@ class Requirement < ApplicationRecord
   
   has_rich_text :description
   
-  validates :minimum_amount_of_credits, numericality: { greater_than: 0 }, if: :not_only_description?
+  validates :minimum_amount_of_credits, numericality: { greater_than_or_equal: 0 }, if: :not_only_description?
   validates :operation, presence: true
   validates :course_ids, presence: true, if: :not_only_description?
 
