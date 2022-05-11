0777e2c858926d5ab25f2d305261f34a99911f9c
Smallz2 <dexterarron@gmail.com>
2022-05-10 22:05:02 -0400

    UI Update

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,7 +1,10 @@
 module CoursesHelper
-
   def discontinue_or_continue_course_options(course)
     return link_to link_to "Discontinue Course", discontinue_course_path(course), method: :patch, class: 'btn btn-danger', data: { confirm: 'Are you sure?' } if !course.discontinued?
     return link_to "Continue Course", continue_course_path(course), method: :patch, class: 'btn btn-success', data: { confirm: 'Are you sure?' } if course.discontinued?
+  end
+
+  def total_credits(course_ids)
+    Course.find(course_ids).pluck(:credit_amount).sum
   end
 end
